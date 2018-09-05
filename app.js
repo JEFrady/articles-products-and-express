9905991480568d7ef541864ccc29c93344429628
JEFrady <jamie.e.halliday@gmail.com>
2018-09-05 13:30:39 -1000

    MVP

--- app.js
+++ app.js
@@ -16,17 +16,23 @@ app.use(methOver('_method'));
 app.engine('.hbs', expressHbs({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+///HOME//////////////////////////////////////////////////
+app.get('/', (req, res) => {
+    console.log('Render home page')
+    res.render('home')
+})
+
 ///PRODUCT//////////////////////////////////////////////////
 
     /// Inventory
     const ProductInventory = require('./db/products.js');
     const prodInv = new ProductInventory();
 
-    //////////HOME//////////
+    //////////Products//////////
     app.get('/products', (req, res) => {
         console.log('Render /products page')
         const items = prodInv.all();
-        res.render('index', { items });
+        res.render('productcat', { items });
     });
 
     //////////NEW PRODUCT FORM//////////
@@ -41,7 +47,7 @@ app.set('view engine', '.hbs');
         const { id } = req.params;
         const item = prodInv.getItemById(id);
         console.log('Product', item);
-        res.render('products', item);
+        res.render('product', item);
     });
 
     //////////EDIT//////////
@@ -84,6 +90,74 @@ app.set('view engine', '.hbs');
     });
 
 ///ARTICLES//////////////////////////////////////////////////
+
+    /// Inventory
+    const ArticleInventory = require('./db/articles.js');
+    const artInv = new ArticleInventory();
+
+    //////////ARTICLES//////////
+    app.get('/articles', (req, res) => {
+        console.log('Render /articles page')
+        const items = artInv.all();
+        res.render('articlecat', { items });
+    });
+
+    //////////NEW ARTICLE FORM//////////
+    app.get('/articles/new', (req, res) => {
+        console.log('Render /articles/new page')
+        res.render('newArt');
+    });
+
+    //////////ARTICLE DETAIL PAGE//////////
+    app.get('/articles/:title', (req, res) => {
+        console.log('Render /articles/:title page')
+        const { title } = req.params;
+        const item = artInv.getItemByTitle(title);
+        console.log('Article', item.title);
+        res.render('article', item);
+    });
+
+    //////////EDIT//////////
+    app.get('/articles/:title/edit', (req, res) => {
+        console.log('Render /articles/:title/edit page')
+        const { title } = req.params;
+        const item = artInv.getItemByTitle(title);
+        // console.log('Product', item);
+        res.render('editArt', item);
+
+    });
+
+    //////////CREATE ARTICLE//////////
+    app.post('/articles/new', (req, res) => {
+        const item = req.body;
+        if (item.title !== '' && item.author !== '' && item.body !== '') {
+            artInv.add(item);
+            res.redirect('/articles')  
+        }
+        else {
+            console.log('Unknown error')
+        }
+    });
+
+    //////////EDIT PRODUCT//////////
+    app.put('/articles/:title/edit', (req, res) => {
+        const { title } = req.params;
+        const item = artInv.updateItemByTitle(title, req);
+
+        console.log('Put', item)
+
+        res.redirect('/articles') 
+    });
+
+    //////////DELETE PRODUCT//////////
+    app.delete('/articles/:title', (req, res) => {
+        const { title } = req.params;
+        // console.log(title)
+        const item = artInv.deleteItemByTitle(title);
+        res.redirect('/articles')
+    });    
+
+
 
 /// LISTEN
 app.listen( PORT, () => {
--- db/articles.js
+++ db/articles.js
@@ -1,10 +1,45 @@
-class Inventory {
+class ArticleInventory {
     constructor(){
-        this._count = 1;
-        this._storage = [];
+        this._count = 3;
+        this._storage = [
+            {title: 'Selling oranges', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta pretium ipsum ut pretium. Aliquam aliquet tincidunt cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi posuere aliquam fringilla. Nullam eu mauris varius, rutrum lectus et, sagittis mauris. Sed in lacus ac magna faucibus ultrices id at erat. Phasellus pulvinar sed nisi eu volutpat. Curabitur interdum, lacus sed hendrerit imperdiet, erat risus fermentum magna, vitae molestie nisi leo iaculis odio. Phasellus vitae lacus metus. Pellentesque non faucibus neque. Sed commodo magna risus, vel sodales nunc fermentum vel. Nam mattis aliquam nisl non sodales.Proin tempus diam ac fermentum finibus. Etiam lacinia arcu quis justo porttitor scelerisque. Suspendisse laoreet maximus sapien. In hac habitasse platea dictumst. Donec sed mollis metus, id viverra arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec neque nibh, condimentum vitae dapibus sed, mollis in ipsum. Sed efficitur volutpat turpis nec vestibulum. Curabitur eleifend sit amet odio non pharetra. Phasellus egestas vestibulum sollicitudin. Donec ornare tempor tortor, vel ultrices dui efficitur id.', author: 'Bob the fruit guy', urlTitle: 'TBD', id: 1}, 
+            {title: 'The life of a shop owner', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta pretium ipsum ut pretium. Aliquam aliquet tincidunt cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi posuere aliquam fringilla. Nullam eu mauris varius, rutrum lectus et, sagittis mauris. Sed in lacus ac magna faucibus ultrices id at erat. Phasellus pulvinar sed nisi eu volutpat. Curabitur interdum, lacus sed hendrerit imperdiet, erat risus fermentum magna, vitae molestie nisi leo iaculis odio. Phasellus vitae lacus metus. Pellentesque non faucibus neque. Sed commodo magna risus, vel sodales nunc fermentum vel. Nam mattis aliquam nisl non sodales.Proin tempus diam ac fermentum finibus. Etiam lacinia arcu quis justo porttitor scelerisque. Suspendisse laoreet maximus sapien. In hac habitasse platea dictumst. Donec sed mollis metus, id viverra arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec neque nibh, condimentum vitae dapibus sed, mollis in ipsum. Sed efficitur volutpat turpis nec vestibulum. Curabitur eleifend sit amet odio non pharetra. Phasellus egestas vestibulum sollicitudin. Donec ornare tempor tortor, vel ultrices dui efficitur id.', author: 'Sally the Store Owner', urlTitle: 'TBD', id: 2}];
     }
     all() {
         return [... this._storage]
         
     }
+    getItemByTitle(title) {
+        let article = this._storage.filter(item => title == item.title);
+        if (article.length > 0) {
+            return article[0]
+        }
+    }
+    add(item) {
+        item.id = this._count;
+        this._storage.push(item);
+        this._count++;
+        return item.id;
+    }
+    updateItemByTitle(title, req) {
+        let temp = this._storage.filter(item => title == item.title);
+        let article = temp[0]
+        article.title = req.body.title
+        article.author = req.body.author
+        article.body = req.body.body
+
+        return article
+    }
+    deleteItemByTitle(title) {
+        let storage = this._storage
+        let article = this._storage.filter(item => title == item.title);
+        for(let i=0; i<storage.length; i++){
+            if (storage[i].title == title) {  
+                storage.splice(i, 1)
+                return storage
+            }
+        }
+
+    }
 }
+module.exports = ArticleInventory;
